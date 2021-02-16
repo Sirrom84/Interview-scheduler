50daa10c0071c8e45c3f67b438e5a2769e052511
Sirrom84 <morrisrjc@gmail.com>
2021-02-15 19:13:10 -0700

    added DayList component and tests for the functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+import classNames from "classnames";
 import "components/Button.scss";
-import classnames from "classnames";
+// import {action} from "@storybook/addon-actions";
 
 export default function Button(props) {
-	const buttonClass = classnames("button", {
+	const buttonClass = classNames("button", {
 		"button--confirm": props.confirm,
 		"button--danger": props.danger,
 	});
